effabb40742129c18ab1b4352e80e2c5b7b74579
Tom Regan <code.tom.regan@gmail.com>
2013-02-17 16:46:06 +0000

    adds some implementation for epicFail; removes the global it

--- verify.py
+++ verify.py
@@ -1,7 +1,5 @@
 import zipfile, tempfile, os, shutil
 
-it = None
-
 class Member(object):
     def __init__(self, parent, membername):
         self._archive = parent
@@ -31,8 +29,11 @@ class Member(object):
 class Archive(object):
     def __init__(self, filename):
         self._archive = zipfile.ZipFile(filename)
+        self._epicFail = False
 
     def shouldContain(self, membername):
+        if self._epicFail:
+            self._expectedContents.append(membername)
         self._archive.getinfo(membername)
         return Member(self._archive, membername)
 
@@ -42,21 +43,24 @@ class Archive(object):
         return self
 
     def epicFail(self):
-        pass
+        if not hasattr(self, '_expectedContents'):
+            self._expectedContents = []
+        self._epicFail = True
+        return self
 
 def verify(filename):
-    global it
     it = Archive(filename)
     return it
 
 
 if __name__ == '__main__':
-    verify('test.zip').epicFail()
+    it = verify('test.zip').epicFail()
     it.shouldContain('1.txt').withText("ohai").withText("ima kitteh")
     it.shouldContain('2.txt')
     it.shouldContain('3.txt')
     it.shouldContain('dir/a.txt')
     it.doesNotContain('nx.txt')
+    print it._expectedContents
 
     jar = it.shouldContain('FakeJar.jar')
     jar.shouldContain("MANIFEST.MF").withText("ohai")
